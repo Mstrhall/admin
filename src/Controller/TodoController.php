a13e3877392792ad83bc6e3e8d120feda7b3f2ca
celian laumond <celian.laumond@gmail.com>
2023-06-17 11:32:42 +0200

    Mettre ici votre message de commit

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -21,12 +21,30 @@ class TodoController extends AbstractController
                 'travail'=>'trouver une alternance'
             ];
             $session->set('tableau',$tableau);
+            $this->addFlash('info',"la liste des todo vien d'etre initialiser");
         }
         return $this->render('todo/index.html.twig');
     }
-    #[Route('/todo/{cle}/{element}', name: 'app_todo')]
+    #[Route('/todo/{cle}/{element}', name: 'app_add_todo')]
     public function addtodo(Request $request,$cle,$element){
-        $session=$request->hasSession()
+        $session=$request->getSession();
+        if ($session->has('tableau')){
+            $tableau=$session->get('tableau');
+            if(isset ($tableau[$cle])){
+                $this->addFlash('erreur',"une cle :$cle  a deja etait crée ");
+
+            }else{
+                $tableau[$cle]=$element;
+                $session->set('tableau',$tableau);
+                $this->addFlash('succes',"la $cle a recu $element" );
+                return $this->redirectToRoute('app_todo');
+            }
+
+        }
+        else{
+            $this->addFlash('erreur',"il n'y a pas de tableau en variable de sesion");
+        }
+        return $this->redirectToRoute('app_todo');
 
 
     }
