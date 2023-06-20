ffb8aed5f1a6f6369756ef82f4ce3225e9872c2b
Mstrhall <celian.laumond@gmail.com>
2023-06-20 17:51:28 +0200

    debut de la gestion utilisateur importantion de mon template plus modificatioon du tableau utilisateur

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,28 +2,43 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FirstController extends AbstractController
 {
+    #[Route('/template', name: 'template')]
+    public function template(): Response
+    {
+
+        return $this->render('template.html.twig'
+
+
+        );
+    }
     #[Route('/first/celian', name: 'app_first')]
     public function index(): Response
     {
 
         return $this->render('first/index.html.twig', [
             'name'=>'laumond',
-            'firstName'=>'celian'
+            'firstName'=>'celian',
+
 
         ]);
     }
-    #[Route('/bonjour/{name}', name: 'bonjour')]
-    public function bonjour($name):Response
-    {
-        return $this->render('first/bonjour.html.twig',['nom'=>$name]);
 
+    public function bonjour(Request $request, $name, string $prenom): Response
+    {
+        return $this->render('first/bonjour.html.twig', [
+            'nom' => $name,
+            'prenom' => $prenom
+        ]);
     }
+
     #[Route('/multiplication/{entier1<\d+>}/{entier2<\d+>}', name: 'multi')]
     public function multiplication($entier1,$entier2){
         $resultat=$entier1*$entier2;
--- src/Controller/TabController.php
+++ src/Controller/TabController.php
@@ -8,16 +8,29 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TabController extends AbstractController
 {
-    #[Route('/tab/{nb?5}', name: 'app_tab')]
+    #[Route('/tab/lol/{nb?5}', name: 'app_tab')]
     public function index($nb): Response
     {
         $tab=[];
         for($i=0;$i<$nb;$i++){
-            $tab[$i]=rand();
+            $tab[$i]=rand(1,4);
         }
         return $this->render('tab/index.html.twig', [
             'controller_name' => 'TabController',
             'tab'=>$tab
+        ]);
+    }
+    #[Route('/tab/user', name: 'user')]
+    public function user(): Response
+    {
+        $user=[
+            ['firstname'=>'celian', 'age'=>'20', 'name'=>'laumond'],
+            ['firstname'=>'andrea', 'age'=>'20', 'name'=>'lajou'],
+            ['firstname'=>'matthieu', 'age'=>'19', 'name'=>'ruans'],
+            ['firstname'=>'nathan', 'age'=>'20', 'name'=>'chaize'],
+        ];
+        return $this->render('tab/users.html.twig',[
+            'users'=>$user
         ]);
     }
 }
