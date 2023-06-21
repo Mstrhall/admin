6a1d768e11337e8cd09fb7080856795d4f0bf296
Mstrhall <celian.laumond@gmail.com>
2023-06-21 12:03:53 +0200

    recuperation de données dans la bd pus implementation avec un fakers

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/personne')]
 class PersonneController extends AbstractController
 {
+    #[Route('/', name: 'personne.list')]
+    public function index(ManagerRegistry $doctrine){
+        $repository=$doctrine->getRepository(Personne::class);
+        $personne=$repository->findAll();
+        return $this->render('personne/index.html.twig',['personne'=>$personne]);
+
+    }
     #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
+        $entityManager=$doctrine->getManager();
+        $personne=new Personne();
+        $personne->setPrenom('celian');
+        $personne->setNom('laumond');
+        $personne->setAge('20');
+
+        $personne2=new Personne();
+        $personne2->setPrenom('philippe');
+        $personne2->setNom('laumond');
+        $personne2->setAge('53');
+
+        //ajouter l'operation d'insertion de la personne dans ma transaction
+        $entityManager->persist($personne);
+        $entityManager->persist($personne2);
+        //execution de la requete
+        $entityManager->flush();
+        return $this->render('personne/detail.html.twig', [
             'controller_name' => 'PersonneController',
+            'personne'=>$personne
         ]);
     }
 }
