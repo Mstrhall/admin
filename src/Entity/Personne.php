dc074cde778746516f04788cff8d9bb808f437a1
Mstrhall <celian.laumond@gmail.com>
2023-06-21 10:51:23 +0200

    doctrines generations des entités et création des migration

---
+++ src/Entity/Personne.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonneRepository::class)]
+class Personne
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nom = null;
+    #[ORM\Column(type: 'smallint')]
+    private $age;
+
+    #[ORM\Column(length: 70, nullable: true)]
+    private ?string $travail = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(?int $age): void
+    {
+        $this->age = $age;
+    }
+
+    public function getTravail(): ?string
+    {
+        return $this->travail;
+    }
+
+    public function setTravail(?string $travail): static
+    {
+        $this->travail = $travail;
+
+        return $this;
+    }
+}
